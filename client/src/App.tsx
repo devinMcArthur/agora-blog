9825576c280f07ad5c0c4c003ae98eac8e7ced15
devinMcArthur <assortedgerm@gmail.com>
2020-12-08 16:09:29 -0700

    Google Analytics 1.2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,21 @@
 import * as React from "react";
 import ReactGA from "react-ga";
+import { createBrowserHistory } from "history";
 
 import Provider from "./components/Providers";
 import Router from "./components/Router";
 import GlobalStyle from "./GlobalStyle";
 
-function App() {
-  ReactGA.initialize("G-Q1KPBQK41S");
+const history = createBrowserHistory();
 
+ReactGA.initialize("G-Q1KPBQK41S");
+
+history.listen((location) => {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+});
+
+function App() {
   return (
     <Provider>
       <GlobalStyle />
