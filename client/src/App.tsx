176174aee09bd9cf41b92169c20b6bbb17d5cfa0
devinMcArthur <assortedgerm@gmail.com>
2020-12-08 14:05:07 -0700

    Google Analytics 1.1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
+import ReactGA from "react-ga";
+
 import Provider from "./components/Providers";
 import Router from "./components/Router";
 import GlobalStyle from "./GlobalStyle";
 
 function App() {
+  ReactGA.initialize("G-Q1KPBQK41S");
+
   return (
     <Provider>
       <GlobalStyle />
