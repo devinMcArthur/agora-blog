cb9a164e72db169bf8b08278c004d1188df1fdbb
devinMcArthur <assortedgerm@gmail.com>
2020-12-08 16:46:05 -0700

    Google Analytics 1.4

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,16 +6,16 @@ import Provider from "./components/Providers";
 import Router from "./components/Router";
 import GlobalStyle from "./GlobalStyle";
 
-const history = createBrowserHistory();
-
-ReactGA.initialize("UA-185033350-1");
-
-history.listen((location) => {
-  ReactGA.set({ page: location.pathname });
-  ReactGA.pageview(location.pathname);
-});
-
 function App() {
+  const history = createBrowserHistory();
+
+  ReactGA.initialize("UA-185033350-1");
+
+  history.listen((location) => {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  });
+
   return (
     <Provider>
       <GlobalStyle />
