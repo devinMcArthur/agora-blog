8fee2e0991a832c2175d7e140662d651aeb02e0e
devinMcArthur <assortedgerm@gmail.com>
2020-12-26 16:21:55 -0700

    Get Production working 1.1

--- api/src/models/__tests__/Page.test.ts
+++ api/src/models/__tests__/Page.test.ts
@@ -38,31 +38,4 @@ afterAll(async (done) => {
   done();
 });
 
-describe("Page", () => {
-  describe("GET", () => {
-    describe("getByID", () => {
-      describe("success", () => {
-        test("should get Page by ID", async () => {
-          const page = await Page.getByID(page_sars_cov_2._id);
-
-          expect(page).toBeDefined();
-          expect(page?._id.toString()).toBe(page_sars_cov_2._id.toString());
-        });
-      });
-    });
-  });
-
-  describe("POPULATE", () => {
-    describe("populateNormal", () => {
-      describe("success", () => {
-        test("page should be properly populated", async (done) => {
-          const populatedPage = await page_sars_cov_2.populateNormal();
-
-          console.log(populatedPage);
-
-          done();
-        });
-      });
-    });
-  });
-});
+describe("Page", () => {});
