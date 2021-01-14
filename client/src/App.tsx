d76946ddb36b93bbcce1844ae390c2b69d6e010d
devinMcArthur <assortedgerm@gmail.com>
2021-01-14 12:53:00 -0700

    Get Kubernetes working in Development

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,8 @@ import Provider from "./components/Providers";
 import Router from "./components/Router";
 
 function App() {
+  console.log(process.env);
+
   return (
     <Provider>
       <Router />
