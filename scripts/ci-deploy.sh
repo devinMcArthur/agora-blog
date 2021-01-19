7429e132901061aad155c8993121d0490c978871
devinMcArthur <assortedgerm@gmail.com>
2021-01-19 14:50:58 -0700

    Implement Nact, k8s deploy

---
+++ scripts/ci-deploy.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# exit script when any command ran here returns with non-zero exit code
+set -e
+
+COMMIT_SHA1=$CIRCLE_SHA1
+
+# We must export it so it's available for envsubst
+export COMMIT_SHA1=$COMMIT_SHA1
+
+# since the only way for envsubst to work on files is using input/output redirection,
+#  it's not possible to do in-place substitution, so we need to save the output to another file
+#  and overwrite the original with that one.
+envsubst <./kube/api-deployment.yml >./kube/api-deployment.yml.out
+mv ./kube/api-deployment.yml.out ./kube/api-deployment.yml
+envsubst <./kube/client-deployment.yml >./kube/client-deployment.yml.out
+mv ./kube/client-deployment.yml.out ./kube/client-deployment.yml
+
+echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
+
+./kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --certificate-authority=cert.crt \
+  --token=$KUBERNETES_TOKEN \
+  apply -f ./k8s/
