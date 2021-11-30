a9f66500078e3c55b0464e895084bfd8e9077a4c
devinMcArthur <assortedgerm@gmail.com>
2021-11-30 11:47:57 -0700

    Fixes for Docker build

--- client/src/forms/user.tsx
+++ client/src/forms/user.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { yupResolver } from "@hookform/resolvers/yup/dist/yup.umd";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import * as yup from "yup";
 
