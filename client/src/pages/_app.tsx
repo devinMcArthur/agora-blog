bc94f9499c20d9056f77774abfb129a1aa6905ff
devinMcArthur <assortedgerm@gmail.com>
2021-12-17 13:37:36 -0700

    Add Google Analytics

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,33 @@
+import React from "react";
+
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import MainPageContainer from "../components/Common/MainPageContainer";
 import Provider from "../components/Providers";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const handleRouterChange = (url: string) => {
+      window.gtag(
+        "config",
+        process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS as string,
+        {
+          page_path: url,
+        }
+      );
+    };
+
+    router.events.on("routeChangeComplete", handleRouterChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
